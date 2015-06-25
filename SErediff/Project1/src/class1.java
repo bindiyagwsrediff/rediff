fd5e54720b36d0de7217835dd6cf04691f9216fe
Dell <Dell@Dell-PC>
2015-06-25 10:21:12 +0530

    done

--- SErediff/Project1/src/class1.java
+++ SErediff/Project1/src/class1.java
@@ -4,7 +4,7 @@ public class class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("rediff ");
+		System.out.println("rediff uu");
 	}
 
 }
