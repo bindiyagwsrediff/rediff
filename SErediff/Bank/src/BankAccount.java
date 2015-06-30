5f98fdc6edcd85f629a57bd3523dec748e63c821
Dell <Dell@Dell-PC>
2015-06-30 15:59:12 +0530

    bank2

--- SErediff/Bank/src/BankAccount.java
+++ SErediff/Bank/src/BankAccount.java
@@ -4,23 +4,25 @@ public class BankAccount {
 	int amt;
 	int bal=100;
 	
-	public void depositCash(int amt){
+	public int depositCash(int amt){
 	System.out.println("Deposited amt is : "+ amt);
 	bal=bal+amt;
 	System.out.println("Your Account bal after deposit is : "+ bal);
+	return bal;
 	}
 		
 	
-	public void withdrawCash(int amt){
+	public int withdrawCash(int amt){
 		
 		System.out.println("withdrawal amt is : "+ amt);
 		if(amt>bal){
-			System.out.println("Your account doesnt have suff bal");	
+			System.out.println("Your account doesnt have suff bal");
+			return bal;
 		}
 		else{
 			bal=bal-amt;
 			System.out.println("Your Account bal after withdrawal is : "+ bal);	
-			
+			return bal;
 		}
 		
 	}
